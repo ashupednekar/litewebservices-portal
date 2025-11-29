8495432d6136e2db617b1da3202a2f9d1563d3f1
ashupednekar <ashutoshpednekar15@gmail.com>
2025-11-29 11:48:22 +0530

    feat: vcs complete

--- internal/repo/auth.go
+++ internal/repo/auth.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/ashupednekar/litewebservices-portal/pkg"
 	"github.com/go-git/go-git/v6"
@@ -39,7 +40,7 @@ func (s *SshAuth) UpdateOptions(options *git.CloneOptions) error {
 }
 
 func (t *TokenAuth) UpdateOptions(options *git.CloneOptions) error {
-	options.Auth = &http.BasicAuth{Password: t.token}
+	options.Auth = &http.BasicAuth{Username: "git", Password: t.token}
 	return nil
 }
 
@@ -47,6 +48,13 @@ func (r *GitRepo) SetupAuth() error {
 	log.Printf("setting up %s auth\n", pkg.Cfg.VcsAuthMode)
 	switch pkg.Cfg.VcsAuthMode{
 	case "ssh":
+    vendor := strings.TrimPrefix(strings.TrimPrefix(pkg.Cfg.VcsVendor, "https://"), "http://")
+		r.options.URL = fmt.Sprintf(
+        "git@%s:%s/%s.git",
+        vendor, 
+        pkg.Cfg.VcsUser,
+        r.project,
+    )
 		auth := &SshAuth{
 			privKey: pkg.Cfg.VcsPrivKeyPath,
 			password: pkg.Cfg.VcsPrivKeyPassword,
